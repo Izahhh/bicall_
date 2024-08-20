951a24db2e885016f4aef6de3d7c5979cf4465aa
Izabela_Lopes <126127652+Izahhh@users.noreply.github.com>
2024-08-20 20:28:21 -0300

    atualização 20/08

--- screens/CadAluno.js
+++ screens/CadAluno.js
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
 import { StyleSheet, View, Text, TouchableOpacity, TextInput, Image, SafeAreaView, KeyboardAvoidingView, Platform, ScrollView } from "react-native";
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { TextInputMask } from "react-native-masked-text"; 
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/FontAwesome'; 
+
 
 const CadAluno = () => {
   const [nome, setNome] = useState("");
+  const [cpf, setCpf] = useState("");
   const [endereco, setEndereco] = useState("");
   const [bairro, setBairro] = useState("");
   const [errorEnd, setErrorEnd] = useState("");  
   const [errorNum, setErrorNum] = useState("");
   const [errorNome, setErrorNome] = useState("");
+  const [errorCpf, setErrorCpf] = useState("");
   const [numEnd, setNumEnd] = useState("");
   const [cep, setCep] = useState("");
+
+  const navigation = useNavigation();
 
   const buscarCEP = async () => {
     try {
@@ -34,6 +41,7 @@ const CadAluno = () => {
     setErrorNome("");
     setErrorEnd("");
     setErrorNum("");
+    setErrorCpf("");
 
     if (nome.trim() === "") {
       setErrorNome("Por favor, insira o nome do aluno");
@@ -45,6 +53,10 @@ const CadAluno = () => {
     }
     if (numEnd.trim() === "") {
       setErrorNum("Por favor, insira o número da casa");
+      error = true;
+    }
+    if (cpf.trim() === "" || cpf.length < 14) {
+      setErrorCpf("Por favor, insira um CPF válido");
       error = true;
     }
     
@@ -60,7 +72,15 @@ const CadAluno = () => {
     >
       <ScrollView contentContainerStyle={styles.scrollView}>
         <SafeAreaView style={styles.banner}>
-          <Text style={styles.titulo}>Cadastrar Aluno</Text>
+          <TouchableOpacity style={styles.voltarIconContainer} onPress={() => navigation.goBack()}>
+            <Image
+              source={require('../assets/imgs/voltar.png')} 
+              style={styles.voltar}
+            />
+          </TouchableOpacity>
+          <Text style={styles.titulo}>   C A D A S T R A R </Text>
+          <Text style={styles.subTi}>   A L U N O </Text>
+
           <Image
             source={require('../assets/imgs/alunoImg.png')}
             style={styles.image}
@@ -71,10 +91,20 @@ const CadAluno = () => {
           {errorNome ? <Text style={styles.errorMessage}>{errorNome}</Text> : null}
           <TextInput
             style={[styles.input, { borderColor: errorNome ? 'red' : '#40A2E3' }]}
-            placeholder="Nome Completo"
+            placeholder="Nome Aluno (a) Completo"
             placeholderTextColor="#000"
             value={nome}
             onChangeText={setNome}
+          />
+
+          {errorCpf ? <Text style={styles.errorMessage}>{errorCpf}</Text> : null}
+          <TextInputMask
+            type={'cpf'}
+            style={[styles.txtCPF, { borderColor: errorCpf ? 'red' : '#40A2E3' }]}
+            placeholder="CPF"
+            placeholderTextColor="#000"
+            value={cpf}
+            onChangeText={setCpf}
           />
 
           <TextInput
@@ -134,19 +164,48 @@ const styles = StyleSheet.create({
   banner: {
     alignItems: 'center',
     marginBottom: 20,
+    position: 'relative',
   },
   image: {
     marginTop: 40,
-    width: 300,
-    height: 300,
+    width: 200,
+    height: 120,
   },
   titulo: {
-    marginTop: 30,
+    marginTop: 50,
     fontSize: 30,
     color: '#FFA404',
     marginBottom: 20,
     textAlign: 'center',
     fontWeight: 'bold',
+  },
+  subTi: {
+    fontSize: 25,
+    color: '#FFA404',
+    marginBottom:0,
+    textAlign: 'center',
+    marginTop: -7,
+  },
+  infoIconContainer: {
+    position: 'absolute',
+    bottom: 8,
+    right: 10,
+    padding: 10,
+    zIndex: 2,
+  },
+  infoIcon: {
+    width: 30,
+    height: 30,
+  },
+  voltarIconContainer: {
+    position: 'absolute',
+    top: 130, // ajustar altura
+    left:10,
+    zIndex: 2,
+  },
+  voltar: {
+    width: 20,
+    height: 20,
   },
   form: {
     flex: 1,
@@ -178,6 +237,17 @@ const styles = StyleSheet.create({
     fontSize: 16,
     borderColor: '#40A2E3',
     borderWidth: 1,
+  },
+  txtCPF: {
+    flex: 3,
+    height: 50,
+    backgroundColor: '#FFF',
+    borderRadius: 25,
+    paddingLeft: 20,
+    marginBottom: 20,
+    fontSize: 16,
+    borderColor: '#40A2E3',
+    borderWidth: 10,
   },
   inputNum: {
     flex: 1,
